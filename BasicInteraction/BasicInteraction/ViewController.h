e36b42e514e4ce3b76d3ccb87c966e0929fccb76
Onur Baykal <theaob@gmail.com>
2012-11-17 13:26:39 +0200

    added interaction project for IBAction and UI properties

---
+++ BasicInteraction/BasicInteraction/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  BasicInteraction
+//
+//  Created by Onur Baykal on 17.11.2012.
+//  Copyright (c) 2012 Onur Baykal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+-(IBAction)changeLabel:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *theLabel;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+
+@end
---
+++ BasicInteraction/BasicInteraction/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  BasicInteraction
+//
+//  Created by Onur Baykal on 17.11.2012.
+//  Copyright (c) 2012 Onur Baykal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@synthesize theLabel;
+@synthesize textField;
+
+-(IBAction)changeLabel:(id)sender
+{
+    [theLabel setText:
+     [[NSString alloc]
+      initWithFormat:@"Hello, %@",
+      [textField text]]];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
