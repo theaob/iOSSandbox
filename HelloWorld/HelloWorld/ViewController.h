8b54b1b9fa2967854c08b476aba9cd225d59cd7d
Onur Baykal <theaob@gmail.com>
2012-11-17 12:30:59 +0200

    init

---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Onur Baykal on 17.11.2012.
+//  Copyright (c) 2012 Onur Baykal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by Onur Baykal on 17.11.2012.
+//  Copyright (c) 2012 Onur Baykal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
